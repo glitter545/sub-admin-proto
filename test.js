e6d4ec4c265023ece7d8cdd0279683a7a60e558a
glitter545 <bobbybroken992@gmail.com>
2023-09-08 05:09:10 -0700

    removed example data

--- test.js
+++ test.js
@@ -1,46 +1,46 @@
-const people = [
-  {
-    name: "Ali Bin Ahmad",
-    address: "123 Jalan ABC, Taman XYZ, 12345 Kuala Lumpur",
-    phone: "012-3456789",
-    email: "ali@example.com",
-    picture: "./pictures/ali.png",
-    database: 5
-  },
-  {
-    name: "Ahmad Bin Ismail",
-    address: "789 Jalan GHI, Taman RST, 54321 Shah Alam",
-    phone: "017-1234567",
-    email: "ahmad@example.com",
-    picture: "./pictures/ali.png",
-    database: 2
-  },
-  {
-    name: "Khairul Azman",
-    address: "246 Jalan STU, Taman VWX, 77777 Shah Alam",
-    phone: "016-9871234",
-    email: "khairul@example.com",
-    picture: "./pictures/ali.png",
-    database: 4
-  },
-  {
-    name: "Siti Binti Kamal",
-    address: "456 Jalan DEF, Taman UVW, 67890 Petaling Jaya",
-    phone: "011-9876543",
-    email: "siti@example.com",
-    picture: "./pictures/ali.png",
-    database: 3
-  },
-  {
-    name: "Nurul Huda",
-    address: "101 Lorong MNO, Taman PQR, 88888 Subang Jaya",
-    phone: "019-5554443",
-    email: "nurul@example.com",
-    picture: "./pictures/ali.png",
-    database: 6
-  }
-  // ... (other people data)
-];
+// const people = [
+//   {
+//     name: "Ali Bin Ahmad",
+//     address: "123 Jalan ABC, Taman XYZ, 12345 Kuala Lumpur",
+//     phone: "012-3456789",
+//     email: "ali@example.com",
+//     picture: "./pictures/ali.png",
+//     database: 5
+//   },
+//   {
+//     name: "Ahmad Bin Ismail",
+//     address: "789 Jalan GHI, Taman RST, 54321 Shah Alam",
+//     phone: "017-1234567",
+//     email: "ahmad@example.com",
+//     picture: "./pictures/ali.png",
+//     database: 2
+//   },
+//   {
+//     name: "Khairul Azman",
+//     address: "246 Jalan STU, Taman VWX, 77777 Shah Alam",
+//     phone: "016-9871234",
+//     email: "khairul@example.com",
+//     picture: "./pictures/ali.png",
+//     database: 4
+//   },
+//   {
+//     name: "Siti Binti Kamal",
+//     address: "456 Jalan DEF, Taman UVW, 67890 Petaling Jaya",
+//     phone: "011-9876543",
+//     email: "siti@example.com",
+//     picture: "./pictures/ali.png",
+//     database: 3
+//   },
+//   {
+//     name: "Nurul Huda",
+//     address: "101 Lorong MNO, Taman PQR, 88888 Subang Jaya",
+//     phone: "019-5554443",
+//     email: "nurul@example.com",
+//     picture: "./pictures/ali.png",
+//     database: 6
+//   }
+//   // ... (other people data)
+// ];
 
 const allNames = people.map(pro => pro.name);
 const searchInput = document.getElementById('searchInput');
